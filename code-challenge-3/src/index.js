ac062d44a2cf277250538d34810d189e5e86fa2c
m11toh <martohmashah@gmail.com>
2024-04-14 13:56:09 +0300

    deploy

--- code-challenge-3/src/index.js
+++ code-challenge-3/src/index.js
@@ -2,12 +2,13 @@
 
 
 document.addEventListener('DOMContentloaded',(event) => {
-    getMovies()
+    getMovies();
+   
 })
 
 function getMovies(){
    const movielist = document.getElementById('films')
-   fetch("http://localhost:4001/films").then(response => response.json())
+   fetch("https://json-server-r4lw.onrender.com/films").then(response => response.json())
    .then((data) => {
         data.forEach(film =>{
             const list = document.createElement('li')
@@ -37,26 +38,20 @@ function displaymovieinfo(film){
         title.innerText = `${film.title}`;
 
     const description = document.getElementById('film-info')
-        filminfo.innerText = `${film.description}`
+        description.innerText = `${film.description}`;
 
     const runtime = document.getElementById('runtime')
-        runtime.innerText = ` Runtime ${film.runtime}`
+        runtime.innerText = ` Runtime = ${film.runtime} minutes`
 
     const showtime = document.getElementById('showtime')
         showtime.innerText = `${film.showtime}`
 
-    const tickets = document.getElementById('tickets')
-        tickets.innerText = `${film.tickets}`
+    const tickets = document.getElementById('ticket-num')
+        tickets.innerText = `${film.capacity - film.tickets_sold }`
     
-    const availatbletickets = film.capacity - film.tickets_sold;
 }
+
 
 displaymovieinfo(film);
 
-const placeholder = film.map((film) =>{
-    return
-    `
-    
 
-    `
-})
